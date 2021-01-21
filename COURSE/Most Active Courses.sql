9517e5027327068804123c8595695b3a981bd35a
marghar <77785369+marghar@users.noreply.github.com>
2021-01-21 15:26:30 +0200

    Update Most Active Courses.sql

--- COURSE/Most Active Courses.sql
+++ COURSE/Most Active Courses.sql
@@ -1,5 +1,4 @@
 SELECT COUNT(l.id) hits, l.course courseId, c.fullname coursename
-FROM prefix_log l INNER JOIN prefix_course c ON l.course = c.id 
-%%FILTER_COURSES:l.course%% 
+FROM prefix_log l INNER JOIN prefix_course c ON l.course = c.id
 GROUP BY courseId, coursename
 ORDER BY hits DESC
