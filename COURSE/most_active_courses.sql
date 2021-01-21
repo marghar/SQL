0db2bcc5d03a4a0e973c272fd9acb5c93dc76a9a
marghar <77785369+marghar@users.noreply.github.com>
2021-01-21 15:42:55 +0200

    Update most_active_courses.sql

--- COURSE/most_active_courses.sql
+++ COURSE/most_active_courses.sql
@@ -1,2 +1,6 @@
-SELECT c.fullname coursename
-FROM mdl_course c
+Most active courses ###
+Most active courses ###
+SELECT COUNT(l.id) hits, l.course courseId, c.fullname coursename
+FROM prefix_log l INNER JOIN prefix_course c ON l.course = c.id
+GROUP BY courseId, coursename
+ORDER BY hits DESC
